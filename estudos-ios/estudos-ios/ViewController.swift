ad595a602b1675c7586882b61ce2a519bc87130c
Walter Oliveira <walteroliveira@Walters-Mac-mini.local>
2019-04-08 16:18:04 -0300

    add base project for future branches

---
+++ estudos-ios/estudos-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  estudos-ios
+//
+//  Created by Walter Oliveira on 08/04/19.
+//  Copyright © 2019 Walter Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
